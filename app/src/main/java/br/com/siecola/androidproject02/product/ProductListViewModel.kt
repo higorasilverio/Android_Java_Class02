0b2a8ac7cbbbe26ebd75b8e959b2979581f1b9e5
higorasilverio <higorasilverio@outlook.com>
2020-05-16 17:28:29 -0300

    Initial implementation of the sales API

---
+++ app/src/main/java/br/com/siecola/androidproject02/product/ProductListViewModel.kt
@@ -0,0 +1,56 @@
+package br.com.siecola.androidproject02.product
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.com.siecola.androidproject02.network.Product
+import br.com.siecola.androidproject02.network.SalesApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+private const val TAG = "ProductListViewModel"
+
+class ProductListViewModel : ViewModel() {
+
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    private val _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>>
+        get() = _products
+
+    init {
+        getProducts()
+    }
+
+    private fun getProducts() {
+        Log.i(TAG, "Preparing to request products list")
+
+        coroutineScope.launch {
+            var getProductsDeferred = SalesApi.retrofitService.getProducts()
+            try {
+                Log.i(TAG, "Loading products")
+
+                var productsList = getProductsDeferred.await()
+
+                Log.i(TAG, "Number of products ${productsList.size}")
+
+                _products.value = productsList
+
+            } catch (e: Exception) {
+                Log.i(TAG, "Error: ${e.message}")
+            }
+        }
+
+        Log.i(TAG, "Products list requested")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+}
